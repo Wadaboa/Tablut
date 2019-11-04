25d7bfabf8f117ee1b2fa9bd9bf14742552a7a56
Alessio Falai <falai.alessio@gmail.com>
2019-11-04 09:04:17 +0100

    edit hide server logs script

--- hide_server_logs.sh
+++ hide_server_logs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 logs='logs/'
+garbage='garbage/'
 first_line=$(head -n 1 .git/modules/Server/info/exclude)
-if [ "$first_line" != "$logs" ]; then
+if [ "$first_line" != "$garbage" ]; then
+	>.git/modules/Server/info/exclude
 	value=$(<.git/modules/Server/info/exclude)
 	echo -e "${logs}\n${value}" > .git/modules/Server/info/exclude
+	value=$(<.git/modules/Server/info/exclude)
+	echo -e "${garbage}\n${value}" > .git/modules/Server/info/exclude
 	echo "Server logs hidden."
 else
 	echo "Server logs already hidden."
