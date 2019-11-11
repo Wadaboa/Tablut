ff293a5cba21c11af8359776d7f79f942b03fac1
Leonardo Calbi <leocalbi@gmail.com>
2019-11-11 01:35:36 +0100

    fix test_white.ps1

--- test_white.ps1
+++ test_white.ps1
@@ -1,22 +1,9 @@
 
-cd Server\Tablut\
-ant clean
-ant compile
-Start-Process ant server
-cd ..\..\Client\
-venv\Scripts\activate
-python -m pip install --user player\
-
-$White = @"
-"White" "-d"
-"@
-
-$Black = @""Black""@
-
-Start-Process tablut_player -ArgumentList $White
-Start-Process tablut_player -ArgumentList $Black
-
-cd ..\Server\Tablut\
-
-
-
+ant compile -f .\Server\Tablut\
+Start-Process ant server -f .\Server\Tablut\
+.\Client\venv\Scripts\activate
+if (Get-VirtualEnvName == 'venv') {
+    pip install .\Client\player\
+    Start-Process tablut_player White
+    Start-Process tablut_player Black
+}
